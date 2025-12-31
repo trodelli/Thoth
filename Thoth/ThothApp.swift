478a3b182f1e580102b8862575d08ede7d93d552
thomas <trrodelli@icloud.com>
2025-12-31 15:16:29 +0100

    Initial Commit

---
+++ Thoth/ThothApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThothApp.swift
+//  Thoth
+//
+//  Created by Thomas Rodelli on 31/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThothTests/ThothTests.swift
@@ -0,0 +1,16 @@
+//
+//  ThothTests.swift
+//  ThothTests
+//
+//  Created by Thomas Rodelli on 31/12/2025.
+//
+
+import Testing
+
+struct ThothTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
